777c0819edf3d69ac79a6a6caa808c38a4c98800
GeoffreyPS <geoffreyps@gmail.com>
2016-03-28 07:28:57 -0500

    Include example classes

---
+++ src/main/scala/Example.scala
@@ -0,0 +1,9 @@
+import org.xml.sax.Attributes
+import org.xml.sax.helpers.XMLFilterImpl
+
+/**
+  * Created by Geoff on 3/24/16.
+  */
+class Example extends XMLFilterImpl {
+
+}
---
+++ src/main/scala/SvgApp.scala
@@ -0,0 +1,7 @@
+/**
+  * Created by Geoff on 3/24/16.
+  */
+object SvgApp  extends App {
+
+  println("hello world")
+}
