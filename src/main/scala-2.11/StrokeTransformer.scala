8f133650f7cb22eac81f9b445494100796234a21
Geoff <geoffreyps@gmail.com>
2016-04-14 10:45:53 -0500

    Add regex pattern for extracting units from values

--- src/main/scala-2.11/StrokeTransformer.scala
+++ src/main/scala-2.11/StrokeTransformer.scala
@@ -5,6 +5,9 @@ import scala.xml._
 
 class StrokeTransformer extends scala.xml.transform.RewriteRule {
   override def transform(n: Node): Seq[Node] = n match{
-    case e: Elem if e.attribute
+    case e @ Elem(_, _, attribute("stroke-width"), _, _, child @ _*) =>
   }
+
+  val unitsPattern = """(\d*\.*\d*)(\w*)""".r
+
 }
