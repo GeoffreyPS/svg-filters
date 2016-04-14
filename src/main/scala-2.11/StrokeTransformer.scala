3273dfb44ea4b88076b2533ff014d17922c37d0b
Geoff <geoffreyps@gmail.com>
2016-04-14 15:07:16 -0500

    Add a separateUnits function

--- src/main/scala-2.11/StrokeTransformer.scala
+++ src/main/scala-2.11/StrokeTransformer.scala
@@ -4,10 +4,16 @@
 import scala.xml._
 
 class StrokeTransformer extends scala.xml.transform.RewriteRule {
-  override def transform(n: Node): Seq[Node] = n match{
-    case e @ Elem(_, _, attribute("stroke-width"), _, _, child @ _*) =>
+  override def transform(n: Node): Seq[Node] = n match {
+    case e@Elem(_, _, attribute("stroke-width"), _, _, child@_*) =>
   }
 
   val unitsPattern = """(\d*\.*\d*)(\w*)""".r
+
+  def separateUnits(n: Node): Tuple2[Double, String] =
+    n.text match {
+      case unitsPattern(values, units) => new Tuple2(values.toDouble, units)
+      //case x => x
+    }
 
 }
