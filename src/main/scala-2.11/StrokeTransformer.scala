fdca6f13ed82a066ced8a930cd4ab94316f9a3e2
GeoffreyPS <geoffreyps@gmail.com>
2016-04-14 07:42:30 -0500

    Add a RewriteRule method

--- src/main/scala-2.11/StrokeTransformer.scala
+++ src/main/scala-2.11/StrokeTransformer.scala
@@ -4,5 +4,7 @@
 import scala.xml._
 
 class StrokeTransformer extends scala.xml.transform.RewriteRule {
-
+  override def transform(n: Node): Seq[Node] = n match{
+    case e: Elem if e.attribute
+  }
 }
