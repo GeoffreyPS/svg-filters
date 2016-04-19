4a27aa57c892fe8bc146ea063144f25733163090
Geoff <geoffreyps@gmail.com>
2016-04-19 15:56:48 -0500

    update type signatures so that matching clauses actually match

--- src/main/scala/StrokeTransformer.scala
+++ src/main/scala/StrokeTransformer.scala
@@ -6,16 +6,15 @@ import scala.xml._
 
 class StrokeTransformer extends scala.xml.transform.RewriteRule {
   def transform(n: Node, m: Double): Node = n match {
-    case elem @ Node(_, _, Attribute("stroke-width", _, _), _, _, child @ _*) =>
+    case elem @ Elem(_, _, Attribute("stroke-width", _, _), _, _, child @ _*) =>
       val el = updateStroke(elem, m)
       el.copy(child = child map (c => transform(c, m)))
-    case elem @ Node(_, _, _, _, child @ _*) =>
+    case elem @ Elem(_, _, _, _, child @ _*) =>
       elem.asInstanceOf[Elem].copy(child = child map(c => transform(c, m)))
     case other => other
   }
 
   val unitsPattern: Regex = """(\d*\.?\d*)(\w*)""".r
-
 
   def separateUnits(n: Node): Tuple2[Double, String] =
     n.text match {
