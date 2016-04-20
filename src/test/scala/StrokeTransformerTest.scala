7b95791fe719025771000f2ecb84aca80ad8eacf
Geoff <geoffreyps@gmail.com>
2016-04-20 16:36:22 -0500

    Add a basic unit test

--- src/test/scala/StrokeTransformerTest.scala
+++ src/test/scala/StrokeTransformerTest.scala
@@ -2,8 +2,20 @@
   * Test suite for StrokeTransformer
   */
 import org.scalatest._
+import scala.xml.transform.RuleTransformer
 
 class StrokeTransformerTest extends FlatSpec with Matchers {
+
+  val originalXML: scala.xml.Node = <g stroke-width="0.15mm" stroke="#FFFF00">233.11</g>
+
+  "StrokeTransformer" should "multiply stroke-width by given factor" in {
+    val rule = new RuleTransformer(new StrokeTransformer(4))
+    val transformedXML = rule.transform(originalXML)
+    val newStrokeWidth = transformedXML \ "@stroke-width"
+
+    assert(newStrokeWidth.text == "0.6mm")
+  }
+
 
   "The truth" should "be true" in {
     assert(true == true)
