c92a0d11f4b3f5464be0c060504a44846ac45868
Geoff <geoffreyps@gmail.com>
2016-04-26 16:11:51 -0500

    Include an overridden transform method

--- src/main/scala/FillTransformer.scala
+++ src/main/scala/FillTransformer.scala
@@ -5,6 +5,15 @@ import scala.xml._
 
 class FillTransformer(color: String, set: Set[String]) extends scala.xml.transform.RewriteRule {
 
+  override def transform(n: Node): Seq[Node] = {
+    val d = (n \ "@id").text
+    n match {
+      case set contains d =>
+        addFill(elem, color)
+      case other => other
+    }
+  }
+
   def addFill(n: Node, color: String): Elem = {
     n.asInstanceOf[Elem] % Attribute("fill", Text(color), Null)
   }
