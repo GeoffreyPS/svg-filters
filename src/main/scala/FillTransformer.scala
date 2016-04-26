4463038d6f12f0dee45f7e890c97c325499863cb
Geoff <geoffreyps@gmail.com>
2016-04-26 16:11:21 -0500

    Add a function to add fill attribute to XML elements

--- src/main/scala/FillTransformer.scala
+++ src/main/scala/FillTransformer.scala
@@ -4,6 +4,8 @@
 import scala.xml._
 
 class FillTransformer(color: String, set: Set[String]) extends scala.xml.transform.RewriteRule {
-  
+
+  def addFill(n: Node, color: String): Elem = {
+    n.asInstanceOf[Elem] % Attribute("fill", Text(color), Null)
   }
 }
