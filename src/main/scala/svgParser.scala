49fd3988b36bf37499ec5feb8cb9ff1ff2b55f5e
Geoff <geoffreyps@gmail.com>
2016-05-02 14:58:38 -0500

    Include companion object for instantiation

--- src/main/scala/svgParser.scala
+++ src/main/scala/svgParser.scala
@@ -1,7 +1,7 @@
+import java.io.File
 import scala.io.Source
 import scala.xml.{MetaData, NamespaceBinding, NodeSeq}
 import scala.xml.parsing.ConstructingParser
-import scala.xml.transform.RuleTransformer
 
 /**
   * Created by Geoff on 5/1/16.
@@ -11,5 +11,17 @@ class svgParser(input: Source, preserveWS: Boolean, rule: scala.xml.transform.Ru
   override def elem(pos: Int, pre: String, label: String, attrs: MetaData,
     pscope: NamespaceBinding, empty: Boolean, nodes: NodeSeq): NodeSeq = {
       rule.transform(super.elem(pos, pre, label, attrs, pscope, empty, nodes))
+  }
+}
+
+object svgParser {
+  def fromFile(inp: File, preserveWS: Boolean, rule: scala.xml.transform.RuleTransformer) =
+    new svgParser(Source.fromFile(inp), preserveWS, rule).initialize
+
+  def fromSource(inp: Source, preserveWS: Boolean, rule: scala.xml.transform.RuleTransformer) =
+    new svgParser(inp, preserveWS, rule).initialize
+
+  def fromURL(url: String, preserveWS: Boolean, rule: scala.xml.transform.RuleTransformer) = {
+    new svgParser(Source.fromURL(url), preserveWS, rule).initialize
   }
 }
