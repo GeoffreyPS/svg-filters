fa1380ccd6f7ce1ea7db82665da0fb0d03729a05
Geoff <geoffreyps@gmail.com>
2016-05-04 11:41:09 -0500

    Update for demonstration purposes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,16 +6,15 @@ object Main extends App {
   val strokeFactor = 1.5
   val fillColor = "#FFFF99"
   val targetAttribute = "ss:lid"
-  val attributeValues = Set("2")
+  val attributeValues = Set("76", "85", "77")
 
   val stroker = new StrokeTransformer(strokeFactor)
   val filler = FillTransformerGenerator(fillColor, targetAttribute, attributeValues)
 
   val rule = new scala.xml.transform.RuleTransformer(stroker, filler)
 
-  val url = "https://997eaee7450d5da75091-88387ec3e1bbe6f03980edb1f1a8b155.ssl.cf1.rackcdn.com/18a1477616b26aae095c32f3acb2e643594757ac7043ac50b44ed6c0150ef9f7/18a1477616b26aae095c32f3acb2e643594757ac7043ac50b44ed6c0150ef9f7.svg"
-  val path = "./src/test/resources/svg.svg"
-  val parser = svgParser.fromPath(path, false, rule)
+  val url = "https://997eaee7450d5da75091-88387ec3e1bbe6f03980edb1f1a8b155.ssl.cf1.rackcdn.com/e8a5dbf220c10d5ad6bfc63910e98d8592557ca5c7bbb2bd6afe060e9eafe92f/e8a5dbf220c10d5ad6bfc63910e98d8592557ca5c7bbb2bd6afe060e9eafe92f.svg"
+  val parser = svgParser.fromURL(url, false, rule)
   val doc = parser.document()
   val newSVG = doc.docElem
 
